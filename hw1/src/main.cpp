fbf767d97e915b9baf3e78ed411ca60432167886
jonathan-liu-0204 <jonathan.tzuchi.liu@gmail.com>
2022-10-21 13:15:08 +0800

    test tv

--- hw1/src/main.cpp
+++ hw1/src/main.cpp
@@ -120,8 +120,6 @@ void keyCallback(GLFWwindow* window, int key, int, int action, int) {
   }
 }
 
-//test
-
 void initOpenGL() {
   // Initialize OpenGL context, details are wrapped in class.
 #ifdef __APPLE__
