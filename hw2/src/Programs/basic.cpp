f81bb9e59f03dba4a8df04e6cce8bd743bbcc20d
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-11-03 21:40:42 +0800

    TODO 2 ing

--- hw2/src/Programs/basic.cpp
+++ hw2/src/Programs/basic.cpp
@@ -25,6 +25,27 @@ bool BasicProgram::load() {
    *           - glEnableVertexAttribArray
    *           - glVertexAttribPointer
    */
+
+  glGenVertexArrays(num_model, VAO);
+
+  for (int i = 0; i < num_model; i++) {
+    glBindVertexArray(VAO[i]);
+    Model* model = ctx->models[i];
+
+    GLuint VBO[3];
+    glGenBuffers(1, VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * model->positions.size(), model->positions.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * model->normals.size(), model->normals.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * model->texcoords.size(), model->texcoords.data(), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  }
+
   return programId != 0;
 }
 
@@ -52,4 +73,30 @@ void BasicProgram::doMainLoop() {
    *           - glUniform1i
    *           - glDrawArrays
    */
+
+    glUseProgram(programId);
+    int obj_num = (int)ctx->objects.size();
+
+    for (int i = 0; i < obj_num; i++) {
+        int modelIndex = ctx->objects[i]->modelIndex;
+        glBindVertexArray(VAO[modelIndex]);
+
+        Model* model = ctx->models[modelIndex];
+
+        const float* p = ctx->camera->getProjectionMatrix();
+        GLint pmatLoc = glGetUniformLocation(programId, "Projection");
+        glUniformMatrix4fv(pmatLoc, 1, GL_FALSE, p);
+
+        const float* v = ctx->camera->getViewMatrix();
+        GLint vmatLoc = glGetUniformLocation(programId, "ViewMatrix");
+        glUniformMatrix4fv(vmatLoc, 1, GL_FALSE, v);
+
+        const float* m = glm::value_ptr(ctx->objects[i]->transformMatrix * model->modelMatrix);
+        GLint mmatLoc = glGetUniformLocation(programId, "ModelMatrix");
+        glUniformMatrix4fv(mmatLoc, 1, GL_FALSE, m);
+
+        glUniform1i(glGetUniformLocation(programId, "ModelTexture"), 0);
+        glDrawArrays(model->drawMode, 0, model->numVertex);
+    }
+    glUseProgram(0);
 }
