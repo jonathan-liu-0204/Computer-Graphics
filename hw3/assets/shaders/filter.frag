1b7c1d9cc07760433138426d38f0c12876269495
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2022-12-05 16:31:21 +0800

    remove unnecessary codes

--- hw3/assets/shaders/filter.frag
+++ hw3/assets/shaders/filter.frag
@@ -10,14 +10,14 @@ uniform int eanbleGrayscale;
 const float offset = 1.0 / 300;  
 const vec2 offsets[9] = vec2[](
         vec2(-offset,    offset),  // top-left
-        vec2(      0.0f,   offset),  // top-center
-        vec2(  offset,   offset),  // top-right
-        vec2( -offset,      0.0f),    // center-left
-        vec2(      0.0f,      0.0f),    // center-center
-        vec2(  offset,       0.0f),    // center-right
-        vec2( -offset,  -offset), // bottom-left
-        vec2(      0.0f,  -offset), // bottom-center
-        vec2(  offset,   -offset)  // bottom-right
+        vec2(0.0f,         offset),  // top-center
+        vec2(offset,     offset),  // top-right
+        vec2(-offset,    0.0f),    // center-left
+        vec2(0.0f,         0.0f),    // center-center
+        vec2(offset,     0.0f),    // center-right
+        vec2(-offset,   -offset), // bottom-left
+        vec2(0.0f,        -offset), // bottom-center
+        vec2(offset,    -offset)  // bottom-right
     );
 
 float kernel[9] = float[](
@@ -31,7 +31,44 @@ float kernel[9] = float[](
 // For edge detection, you need to apply kernel matrix and sampling offset provided (offsets)
 
 void main(){             
-    color = texture(colorBuffer, TexCoord);
-    float average = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
-    color = vec4(average, average, average, 1.0);
+
+    if((eanbleGrayscale == 1) && (enableEdgeDetection == 1)){
+        vec3 sampleTex[9];
+        for(int i = 0; i < 9; i++){
+            sampleTex[i] = vec3(texture(colorBuffer, TexCoord.st + offsets[i]));
+        }
+
+        vec3 col = vec3(0.0);
+        for(int i = 0; i < 9; i++){
+            col += sampleTex[i] * kernel[i];
+        }
+    
+        color = vec4(col, 1.0);
+        float average = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
+        color = vec4(average, average, average, 1.0);
+    }
+
+    else if(eanbleGrayscale == 1){
+        color = texture(colorBuffer, TexCoord);
+        float average = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
+        color = vec4(average, average, average, 1.0);
+    }
+
+    else if(enableEdgeDetection == 1){
+        vec3 sampleTex[9];
+        for(int i = 0; i < 9; i++){
+            sampleTex[i] = vec3(texture(colorBuffer, TexCoord.st + offsets[i]));
+        }
+
+        vec3 col = vec3(0.0);
+        for(int i = 0; i < 9; i++){
+            col += sampleTex[i] * kernel[i];
+        }
+    
+        color = vec4(col, 1.0);
+    }
+
+    else{
+        color = texture(colorBuffer, TexCoord);
+    }
 }
--- hw3/src/Programs/filter.cpp
+++ hw3/src/Programs/filter.cpp
@@ -10,29 +10,40 @@ FilterProgram::FilterProgram(Context *ctx) : Program(ctx) {
   // TODO#3-1: Generate Framebuffer and VAO/VBO for filter
   // Note:     You need to design proper position/texcoord data for filter program (NDC)
 
-  float quadVertices[] = {   // positions   // texCoords
-                                          -1.0f,  1.0f, 0.0f, 1.0f, 
-                                          -1.0f, -1.0f, 0.0f, 0.0f, 
-                                            1.0f, -1.0f, 1.0f, 0.0f,
+  float quadVertices_pos[] = { -1.0f,  1.0f, 0.0f,
+                                               -1.0f, -1.0f, 0.0f,   
+                                                 1.0f, -1.0f, 0.0f, 
 
-                                          -1.0f,  1.0f, 0.0f, 1.0f, 
-                                           1.0f, -1.0f, 1.0f,  0.0f, 
-                                           1.0f,  1.0f, 1.0f, 1.0f
-                                      };
+                                               -1.0f,  1.0f,  0.0f, 
+                                                1.0f,  -1.0f, 0.0f, 
+                                                1.0f,   1.0f, 0.0f};
+
+  float quadVertices_tex[] = {0.0f, 1.0f, 
+                                             0.0f, 0.0f,
+                                             1.0f, 0.0f,
+
+                                             0.0f, 1.0f,
+                                             1.0f, 0.0f,
+                                             1.0f, 1.0f  };
+
+  glGenFramebuffers(1, &filterFBO);
 
   glGenVertexArrays(1, &quadVAO);
   glGenBuffers(2, quadVBO);
   glBindVertexArray(quadVAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, quadVBO[0]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices_pos), &quadVertices_pos, GL_STATIC_DRAW);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  
   glBindBuffer(GL_ARRAY_BUFFER, quadVBO[1]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices_tex), &quadVertices_tex, GL_STATIC_DRAW);
   glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
   updateFrameBuffer(OpenGLContext::getWidth(), OpenGLContext::getHeight());
 }
@@ -55,31 +66,34 @@ void FilterProgram::updateFrameBuffer(int SCR_WIDTH, int SCR_HEIGHT) {
    *           - glFramebufferRenderbuffer
    */
 
-   unsigned int framebuffer;
-   glGenFramebuffers(1, &framebuffer);
-   glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    // glUseProgram(programId);
 
-   unsigned int colorBuffer;
+   // glGenFramebuffers(1, &filterFBO);
+   glBindFramebuffer(GL_FRAMEBUFFER, filterFBO);
+
    glGenTextures(1, &colorBuffer);
    glBindTexture(GL_TEXTURE_2D, colorBuffer);
+
    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
+
    glBindTexture(GL_TEXTURE_2D, 0);
 
-   unsigned int rbo;
+   GLuint rbo;
    glGenRenderbuffers(1, &rbo);
    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
-   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
 
    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
      std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
    }
    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
 }
 
 void FilterProgram::bindFrameBuffer() {
@@ -93,17 +107,19 @@ void FilterProgram::doMainLoop() {
   /* TODO#3-1: pass VAO, enableEdgeDetection, , colorBuffer to shader and render
    */
 
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glBindVertexArray(quadVAO);
+
     GLint enableEdgeDetection_Loc = glGetUniformLocation(programId, "enableEdgeDetection");
     glUniform1i(enableEdgeDetection_Loc, ctx->enableEdgeDetection);
 
     GLint eanbleGrayscale_Loc = glGetUniformLocation(programId, "eanbleGrayscale");
     glUniform1i(eanbleGrayscale_Loc, ctx->eanbleGrayscale);
 
-    glBindVertexArray(quadVAO);
-    glDisable(GL_DEPTH_TEST);
+    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, colorBuffer);
     glUniform1i(glGetUniformLocation(programId, "colorBuffer"), 0);
-    glDrawArrays(GL_TRIANGLES, 0, 6); 
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glUseProgram(0);
 }
